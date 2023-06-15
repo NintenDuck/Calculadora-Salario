d2a5ecf1ac1f64d2b5a9dac4fcb30c71cd14ad72
NintenDuck <andrecked507@gmail.com>
2023-06-15 12:38:23 -0600

    Add: salary by hour, ext-hour and result from week

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,11 +1,18 @@
 extends Control
 
+
+# INPUTS
 onready var input_salary = get_node( '%InputSalary' )
 onready var input_dias = get_node( '%InputDias' )
 onready var input_extra = get_node( '%InputExtra' )
+
+# BUTTONS
 onready var button_calculate = get_node( '%ButtonCalculate' )
-onready var salario_extra = get_node( '%LabelResultExtra' )
-onready var label_result = get_node( '%LabelResultTotal' )
+
+# LABELS
+onready var lbl_salario_hora = get_node( '%LabelHora' )
+onready var lbl_salario_extra = get_node( '%LabelHoraExtra' )
+onready var lbl_total_semanal = get_node( '%LabelSemanal' )
 
 
 # FUNCIONES BASE
@@ -16,20 +23,24 @@ func _ready() -> void:
 	input_extra.get_line_edit().connect("focus_entered", self, "select_all_text", [input_extra.get_line_edit()])
 
 #FUNCIONES PERSONALES
+# TODO: Hacer que cuando pase de 9 horas cuenta el triple
 func _calculate_salary() -> void:
 	var salario:Dictionary = {
 		'semanal':		0,
+		'hora':			0,
 		'extra':		0,
-		'extra_total':	0,
 	}
 
 	salario.semanal			= input_salary.value
-	salario.extra			= salario.semanal / 48
-	salario.extra_total		= salario.extra * input_dias.value
-
-	salario_extra.text	= str( salario.extra )
-	label_result.text 	= str( salario.semanal + salario.extra_total )
+	salario.hora			= salario.semanal / 48
+	salario.extra			= salario.hora * 2
+	
+	var salario_total		= salario.semanal + ( salario.extra * input_dias.value )
+	
+	lbl_salario_hora.text	= str( salario.hora )
+	lbl_salario_extra.text	= str( salario.extra )
+	lbl_total_semanal.text	= str( salario_total )
 
 func select_all_text( line_node:LineEdit ) -> void:
-	yield( get_tree().create_timer(0.1), "timeout" )
+	yield( get_tree().create_timer(0.05), "timeout" )
 	line_node.select_all()
